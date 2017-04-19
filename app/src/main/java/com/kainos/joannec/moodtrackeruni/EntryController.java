558b084cb9f803a70ff9407be6c7dacfceb3c2a2
joannec <j.connor@kainos.com>
2017-04-19 18:52:40 +0100

    added code for yesterday pie chart

--- app/src/main/java/com/kainos/joannec/moodtrackeruni/EntryController.java
+++ app/src/main/java/com/kainos/joannec/moodtrackeruni/EntryController.java
@@ -81,33 +81,7 @@ public class EntryController extends DatabaseConnection  {
 
         int[] today_data = new int[]{0, 0, 0, 0, 0};
         SQLiteDatabase db = this.getWritableDatabase();
-        /*String sql1 = "select count(moodRating) AS counter from moodEntry where moodRating = 1 and date('now');";
-        String sql2 = "select count(moodRating) from moodEntry where moodRating = 2 and date('now');";
-        String sql3 = "select count(moodRating) from moodEntry where moodRating = 3 and date('now');";
-        String sql4 = "select count(moodRating) from moodEntry where moodRating = 4 and date('now');";
-        String sql5 = "select count(moodRating) from moodEntry where moodRating = 5 and date('now');";
 
-        Cursor cursor1 = db.rawQuery(sql1, null);
-        cursor1.moveToFirst();
-        System.out.println(cursor1.getInt(0));
-        today_data[0] = Integer.parseInt(cursor1.getString(cursor1.getColumnIndex("counter")));
-        cursor1.close();
-     Cursor cursor2 = db.rawQuery(sql2, null);
-        cursor2.moveToFirst();
-        today_data[1] = cursor2.getInt(0);
-        cursor2.close();
-        Cursor cursor3 = db.rawQuery(sql3, null);
-        cursor3.moveToFirst();
-        today_data[2] = cursor3.getInt(0);
-        cursor3.close();
-        Cursor cursor4 = db.rawQuery(sql4, null);
-        cursor4.moveToFirst();
-        today_data[3] = cursor4.getInt(0);
-        cursor4.close();
-        Cursor cursor5 = db.rawQuery(sql5, null);
-        cursor5.moveToFirst();
-        today_data[4] = cursor5.getInt(0);
-        cursor5.close();*/
 
        for (int i = 0; i < 5; i++) {
            int temp = i + 1;
@@ -120,7 +94,23 @@ public class EntryController extends DatabaseConnection  {
        db.close();
         return today_data;
     }
+    public int[] yesterdayPie() {
 
+        int[] yesterday_data = new int[]{0, 0, 0, 0, 0};
+        SQLiteDatabase db = this.getWritableDatabase();
+
+
+        for (int i = 0; i < 5; i++) {
+            int temp = i + 1;
+            String sql = "select count(moodRating) from moodEntry where moodRating = " + temp + " and date('-1 day');";
+            Cursor cursor = db.rawQuery(sql, null);
+            cursor.moveToFirst();
+            yesterday_data[i] = cursor.getInt(0);
+            cursor.close();
+        }
+        db.close();
+        return yesterday_data;
+    }
 
 }
 
